cd94ee98735ff91dde6c1dbb324813815f394e3c
Trikita73 <diachenkoandrii7@gmail.com>
2024-12-15 20:59:03 +0200

    fs

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+// currency_converter
+
+/*
 const CC = require('currency-converter-lt')
 let currencyConverter = new CC({from:"USD", to:"EUR", amount:100})
 
 currencyConverter.convert().then((response) => {
     console.log(`Result: ${response}`)
 })
+*/
 
+// file_systems
+
+// cинхронные функции
+/*
+const fs = require('fs')
+
+let result = fs.readFileSync('some.txt', 'utf-8')
+console.log(result)
+fs.writeFileSync('some.txt', result + '\nHello world!')
+*/
+
+// асинхронные функции
+const fs = require('fs')
+
+fs.readFile('some.txt', 'utf-8', (err, data) => {
+    fs.writeFile('some.txt', data + '\nSome text!', (err, data) => {
+        console.log('Everything work!')
+    })
+})
