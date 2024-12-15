a332761580308480d30e89ed593119bd25373e42
Trikita73 <diachenkoandrii7@gmail.com>
2024-12-15 21:18:40 +0200

    nodules

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ currencyConverter.convert().then((response) => {
 })
 */
 
+
 // file_systems
 
 // cинхронные функции
@@ -21,6 +22,7 @@ fs.writeFileSync('some.txt', result + '\nHello world!')
 */
 
 // асинхронные функции
+/*
 const fs = require('fs')
 
 fs.readFile('some.txt', 'utf-8', (err, data) => {
@@ -28,3 +30,17 @@ fs.readFile('some.txt', 'utf-8', (err, data) => {
         console.log('Everything work!')
     })
 })
+*/
+
+
+// модули
+/*
+const os = require('os')
+let res = os.platform()
+console.log(res)
+
+const my_math = require('./my_math')
+let res1 = my_math.add(4, 5)
+let res2 = my_math.minus(6, 9)
+console.log(`Res1: ${res1}, Res2: ${res2}`)
+*/
