142be922a9cb84c1203b64cb8333b01180e5b2cb
Trikita73 <diachenkoandrii7@gmail.com>
2024-12-18 23:49:44 +0200

    ejs

--- index.js
+++ index.js
@@ -151,3 +151,31 @@ app.listen(PORT, () => {
 })
 */
 
+
+// Шаблонизатор
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/user/:username', (req, res) => {
+    res.render('user', { username: req.params.username })
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`Server started: http://localhost:${PORT}`)
+})
+
