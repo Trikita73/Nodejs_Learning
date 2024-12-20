0cb669f6541e3163413abdbd91c4b5f81ffebbb4
Trikita73 <diachenkoandrii7@gmail.com>
2024-12-21 01:14:17 +0200

    get_post_data

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ app.listen(PORT, () => {
 
 
 // Подключение файлов
-
+/*
 const express = require('express')
 
 const app = express()
@@ -211,4 +211,43 @@ const PORT = 3000
 app.listen(PORT, () => {
     console.log(`Server started: http://localhost:${PORT}`)
 })
+*/
 
+
+// Получение и обработка данных от пользователя
+
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/user/:username', (req, res) => {
+    let data = { username: req.params.username, hobbies: [ 'Football', 'Skate', 'Basketball' ]}
+    res.render('user', data)
+})
+
+app.post('/check-user', (req, res) => {
+    let username = req.body.username
+    if(username == "")
+        return res.redirect('/')
+    else
+        return res.redirect('/user/' + username)
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`Server started: http://localhost:${PORT}`)
+})
