44a2ae30044822fdede1fa096b301deb0dc708ee
Trikita73 <diachenkoandrii7@gmail.com>
2024-12-20 23:54:18 +0200

    include_files

--- index.js
+++ index.js
@@ -153,12 +153,44 @@ app.listen(PORT, () => {
 
 
 // Шаблонизатор
+/*
 const express = require('express')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/user/:username', (req, res) => {
+    let data = { username: req.params.username, hobbies: [ 'Football', 'Skate', 'Basketball' ]}
+    res.render('user', data)
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`Server started: http://localhost:${PORT}`)
+})
+*/
+
+
+// Подключение файлов
+
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
